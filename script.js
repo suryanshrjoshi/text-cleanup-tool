0eb6fadb45818d8fd6696588fbb267684c3999d0
suryanshrjoshi <suryansh.pacific@gmail.com>
2025-05-14 01:10:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ function clearText() {
   document.getElementById('inputText').value = '';
   document.getElementById('outputText').value = '';
 }
+
+function removeExtraSpaces() {
+  const inputText = document.getElementById('inputText').value;
+  const cleanedText = inputText.replace(/\s+/g, ' ').trim();
+  document.getElementById('outputText').value = cleanedText;
+}
+
+function removeLineBreaks() {
+  const inputText = document.getElementById('inputText').value;
+  const cleanedText = inputText.replace(/[\r\n]+/g, ' ').trim();
+  document.getElementById('outputText').value = cleanedText;
+}
